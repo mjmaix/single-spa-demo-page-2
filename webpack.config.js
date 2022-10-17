e5d2dcebd4b34b54b195a21f2182be7a42663b8f
MJ Abadilla <mjmaix@gmail.com>
2022-10-17 23:28:11 +0800

    Add cname generator for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
 
+const CnameWebpackPlugin = require("cname-webpack-plugin");
+
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "mja-org",
@@ -11,5 +13,11 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+
+    plugins: [
+      new CnameWebpackPlugin({
+        domain: process.env.CNAME_VALUE || "example.com",
+      }),
+    ],
   });
 };
